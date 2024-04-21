2119c6a0e78ec862e555ef64aa4482266790cddb
tsyu12345 <syukut@gmail.com>
2024-04-22 00:50:01 +0900

    CUDAインストールのコードを追加

--- init.ps1
+++ init.ps1
@@ -19,3 +19,10 @@ if ($cuda -eq $true) {
     Write-Host "Installing PyTorch with CUDA support..."
     pip install torch==1.13.1+cu116 torchaudio==0.13.1 torchvision==0.14.1+cu116 --index-url https://download.pytorch.org/whl/cu116
 }
+
+
+#CUDAインストーラーの起動
+$installerPath = "./cuda_11.6.0_windows_network.exe"
+Write-Output "Installing CUDA 11.6..."
+Start-Process -FilePath $installerPath -ArgumentList "-s" -Wait
+Write-Output "CUDA 11.6 installation complete."
